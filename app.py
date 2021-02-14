e1bb3e6cba2992fb9e83e90a722954726a60018f
Ishan Srivastava <ishanmay8@gmail.com>
2021-02-14 11:55:48 +0530

    cors support added

--- app.py
+++ app.py
@@ -9,11 +9,31 @@ from sqlalchemy import desc
 from models import Memes
 import uvicorn
 import models
-
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(docs_url='/swagger-ui/', port=8080)
+
+
+# for Cors control
+# enable support for ors
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:52535",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # initilise database
 
